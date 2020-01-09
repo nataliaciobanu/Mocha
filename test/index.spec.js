ee1f0550479fdea5be49b3e1adb9f71f6785764f
tocana_n <tocana_n@yahoo.com>
2020-01-08 20:04:07 -0800

    Unit Testing

---
+++ test/index.spec.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const {sum, mult} = require('../index.js');
+
+describe('function sum', () => {
+    it("should function sum equals 10",() => {
+        assert.equal(sum(2, 4),6 )
+    });
+
+    it("should function sum not equals 0",() => {
+        assert.notEqual(sum(2, 4),0);
+    });
+
+    it('should mult a equals 8', () => {
+        assert.equal(mult(2,4),8);
+    });
+
+    it('should mult a not equals 10', () => {
+        assert.notEqual(mult(2,4),10);
+    });
+});
+
