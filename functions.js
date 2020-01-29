7e685b5a0d70ceb9e9c9990aa585cb622e8c6e5e
tocana_n <tocana_n@yahoo.com>
2020-01-28 23:31:35 -0800

    Unit Testing

--- functions.js
+++ functions.js
@@ -6,7 +6,20 @@ function mult (a, b){
     return a * b;
 }
 
-module.exports = {sum, mult};      // export
+function power (base, exponent) {
+    let result = 1;
+    for(let i = 0; i < exponent; ++i) {
+        result *= base;
+    }
+    return result
+}
+
+function isAdult(age){
+    return age < 0 ? undefined : age >= 21;
+}
+
+
+module.exports = {sum, mult, power, isAdult};      // export
 
 
 
--- test/functions.spec.js
+++ test/functions.spec.js
@@ -1,17 +1,17 @@
-const assert = require('assert')
-const {sum, mult} = require('../functions.js');        //import from functions.js
+const assert = require('assert');
+const {sum, mult, power, isAdult} = require('../functions.js');        //import from functions.js
 
 describe('function sum', () => {
-    it('should  sum  equals 10', () => {
+    it('should  sum  equals 10', function ()  {
         assert.equal(sum(2, 4),6);
-    })
+    });
 
-    it('should  sum not equal 0',  () => {
+    it('should  sum not equal 0',  function ()  {
         assert.notEqual(sum(2, 4),0)
     });
-})
+});
 
-describe('function mult', () => {
+describe('function mult', () =>  {
     it('should  mult a equals 0 ', function () {
         assert.equal(mult(2, 4),8)
     });
@@ -19,33 +19,53 @@ describe('function mult', () => {
     it('should  mult a not equals 8 ', function () {
         assert.equal(mult(2, 4),8)
     });
-})
+});
+
+describe ('function power', () => {
+    it('power should return integer in power integer ', function () {
+        assert.equal(power(10, 2), 100);
+    });
+
+    it('power should return odd integer in power integer', function () {
+        assert.equal(power(-8,2),64)
+    });
+
+    it('power should return decimal in power decimal', function () {
+        assert.equal(power(-2.5, 2),6.25)
+    });
+
+    it('power should return 1', function () {
+        assert.equal(power(0,0),1)
+    });
+
+    it('power should return 0', function () {
+        assert.equal(power(0,1000),0)
+    });
+});
+
+describe('function isAdult', () => {
+    it('isAdult should return true if argument equals 21', function ()  {
+        assert.equal((isAdult(21)), true)
+    });
+
+    it('isAdult should return true if argument is more than 21', function ()  {
+        assert.equal((isAdult(40)), true)
+    });
+
+    it('isAdult should return false if argument is less than 21, but more than 0', function ()  {
+        assert.equal((isAdult(5)), false)
+    });
+
+    it('isAdult should return false if argument equals 0', function ()  {
+        assert.equal((isAdult(0)), false)
+    });
+
+    it('isAdult should return undefined if argument is less than 0', function ()  {
+        assert.equal((isAdult(-10)), undefined)
+    });
+});
 
 
-// const assert = require('assert');
-// //const {expect} = require ('chai');
-//
-// const {sum, mult,} = require('../functions.js');
-//
-// describe('function sum', () => {
-//     it("should function sum equals 10", () => {
-//         assert.equal(sum(2, 4), 6)
-//     });
-//
-//     it("should function sum not equals 0", () => {
-//         assert.notEqual(sum(2, 4), 0);
-//     });
-//
-//     describe('function mult', () => {
-//         it('should mult a equals 8', () => {
-//             assert.equal(mult(2, 4), 8);
-//         });
-//
-//         it('should mult a not equals 10', () => {
-//             assert.notEqual(mult(2, 4), 10);
-//         });
-//     });
-// }
 
 
 
